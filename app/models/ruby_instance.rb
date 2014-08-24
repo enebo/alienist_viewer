a2a511435c51bb68cce18e6008df165e5c86da59
val99erie <val99erie@gmail.com>
2014-08-24 11:11:06 -0500

    Renamed links

--- app/models/ruby_instance.rb
+++ app/models/ruby_instance.rb
@@ -9,4 +9,24 @@ class RubyInstance
   def to_param
     id.to_s
   end
+
+  def display_name
+    "#<#{@cls.name}: 0x#{@id.to_s(16)}>"
+  end
+
+  def display_value
+    case @cls.name
+    when 'NilClass'
+      'nil'
+    when 'FalseClass'
+      'false'
+    when 'TrueClass'
+      'true'
+    when 'String'
+      '"' + @data + '"'
+    else
+      display_name
+    end
+  end
+
 end
